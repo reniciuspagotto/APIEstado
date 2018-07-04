6a6e79e09f53296b80c8ba6e6b51bf238ebe3653
Renícius Pagotto Fostaini <renicius.pagotto@outlook.com>
2018-07-03 22:35:55 -0300

    Add project files.

---
+++ ApiEstado/Controller/EstadoController.cs
@@ -0,0 +1,37 @@
+﻿using ApiEstado.Dto;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ApiEstado.Controller
+{
+    [Route("api/estado")]
+    [ApiController]
+    public class EstadoController : ControllerBase
+    {
+        [HttpGet]
+        public List<EstadoDto> Get()
+        {
+            return ListarEstados();
+        }
+
+        [HttpGet("{id}")]
+        public EstadoDto Get(int id)
+        {
+            var estados = ListarEstados();
+            return estados.FirstOrDefault(x => x.Id == id);
+        }
+
+        private List<EstadoDto> ListarEstados()
+        {
+            var estados = new List<EstadoDto>()
+            {
+                new EstadoDto{ Id = 1, Sigla = "SP", Nome = "São Paulo"},
+                new EstadoDto{ Id = 2, Sigla = "MG", Nome = "Minas Gerais"},
+                new EstadoDto{ Id = 3, Sigla = "RJ", Nome = "Rio de Janeiro"}
+            };
+
+            return estados;
+        }
+    }
+}
---
+++ ApiEstado/Dto/EstadoDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiEstado.Dto
+{
+    public class EstadoDto
+    {
+        public int Id { get; set; }
+        public string Sigla { get; set; }
+        public string Nome { get; set; }
+    }
+}
